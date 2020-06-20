9dd5781c00fe11f43a6ae8f30b38fad35721f860
Boris Shukaylo <sniperjke@gmail.com>
2020-06-21 01:25:07 +0600

    added hook to Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,27 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {AlertContext} from "../context/alert/alertContext";
 
 export const Search = () => {
+    const [value, setValue] = useState('');
     const {show} = useContext(AlertContext);
 
     function onSubmit(event) {
-        if (event.key === 'Enter') {
-            show('this is alert!')
+        if (event.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            //get data from github
+        } else {
+            show("Enter username!")
         }
     }
 
     return (
         <div className='form-group'>
             <input
+                value={value}
+                onChange={event => setValue(event.target.value)}
                 type="text"
                 name="form-control"
                 placeholder='Введите ник пользователя'
